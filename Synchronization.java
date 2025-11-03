a0fd197543ca2afdab8ce0806e6f8268abbb3dd5
ahmedMohamed5886 <hm813935@gmail.com>
2025-11-03 13:52:07 +0200

    Update Synchronization.java

--- Synchronization.java
+++ Synchronization.java
@@ -1,5 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
+
+class Semaphore {
+    private int value;
+
+    public Semaphore(int value) {
+        this.value = value;
+    }
+
+    public synchronized void waitSem() {
+        while (value <= 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        value--;
+    }
+
+    public synchronized void signal() {
+        value++;
+        notify();
+    }
+}
 
 public class Synchronization extends JFrame
 {
